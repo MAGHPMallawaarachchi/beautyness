129fd82c56982240fb556f4c212ea0b415aad944
Hasini Punsala <hasinipunsala@gmail.com>
2023-02-06 21:44:22 +0530

    update features.jsx

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,8 +2,17 @@ import react, {useState} from 'react';
 import styles from '../style';
 import {features} from '../constants';
 
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 const FeatureCard = ({id, image, title, content}) => (
-  <div className='flex flex-row'>
+  <div className='flex-row'>
     <div className='flex flex-col gap-[26px]'>
       <img src={image} alt="" className='min-w-[330px] min-h-[253px]' />
       <h2 className='font-playfair_display font-bold text-dullBlack text-[26px] leading-[120%]'>{title}</h2>
@@ -15,11 +24,25 @@ const FeatureCard = ({id, image, title, content}) => (
 
 const Features = () => (
   <section className={`sm:px-16 ${styles.paddingX} ${styles.paddingY} flex justify-center`}>
-    <div className='gap-[105px] flex'>
+    <div className='md:flex hidden gap-[105px] flex-wrap justify-center'>
       {features.map((feature, index)=>(
         <FeatureCard key={feature.id}{...feature}/>
       ))}
     </div>
+
+    <Swiper 
+      className='flex flex-col md:hidden gap-[105px] items-center justify-center pb-12'
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+    >
+      {features.map((feature, index)=>(
+        <SwiperSlide>
+          <FeatureCard key={feature.id}{...feature}/>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   </section>
 )
 
