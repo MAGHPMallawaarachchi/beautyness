206347d611f5b1a4900b67b037334ccf1b9f3591
Hasini Punsala <hasinipunsala@gmail.com>
2023-02-06 22:14:30 +0530

    update App.jsx, index.js

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,7 @@ const App = () => {
       </div>
     </div>
     <Routes>
-      <Route path="/" element={<Home/>}/>
+      <Route path="/beautyness" element={<Home/>}/>
       <Route path="/about" element={<About/>}/>
       <Route path="/services" element={<Services/>}/>
       <Route path="/contact" element={<Contact/>}/>
