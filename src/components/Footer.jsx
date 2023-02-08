a621f10b129e5c7b53ebc65bd1c2ee6e4ce6250f
Hasini Punsala <hasinipunsala@gmail.com>
2023-02-08 22:25:35 +0530

    update footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import styles from '../style';
+import { logo_header, reservation, telephone, menu, close } from '../assets';
+import { navLinks, socialMedia } from "../constants";
 
 const Footer = () => {
   return (
